d6a21e3d44e5735f4a429dd1e0ac3d8ccedf0144
Abderrahim Jari <ajari@e1r11p9.1337.ma>
2023-04-01 22:16:20 +0000

    set_ajari

--- src/parsing/parsing_util.c
+++ src/parsing/parsing_util.c
@@ -3,18 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing_util.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aahrach <aahrach@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ajari <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/03 20:43:23 by ajari             #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/04/01 20:01:00 by ajari            ###   ########.fr       */
-=======
-<<<<<<< HEAD
-/*   Updated: 2023/04/01 18:19:31 by aahrach          ###   ########.fr       */
-=======
-/*   Updated: 2023/04/01 18:42:22 by ajari            ###   ########.fr       */
->>>>>>> d314fef6c656e3aacafe64e032cca0f4851112c9
->>>>>>> 4ce88c5298f396b92f04ecf993075efb61c76905
+/*   Created: 2023/04/01 22:14:25 by ajari             #+#    #+#             */
+/*   Updated: 2023/04/01 22:16:11 by ajari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +44,6 @@ int	outfd(char *name, int trunc, int *stat)
 {
 	int	fd;
 
-<<<<<<< HEAD
 	fd = -2;
 	name = expend(ft_strdup(name), 0, 1, &fd);
 	if (fd == -1)
@@ -62,17 +53,10 @@ int	outfd(char *name, int trunc, int *stat)
 		if (access(name, W_OK) == -1)
 			return (error("permission denied", name), *stat = 0, -1);
 	}
-=======
-<<<<<<< HEAD
-	if (access(name, W_OK) == -1 && access(name, W_OK) == -1)
-=======
-	name = expend(name, 0, 1, 1);
 	if (access(name, W_OK) == -1)
 		return (error("no such file or directory", name), *stat = 0, 1);
 	if (access(name, W_OK) == -1)
->>>>>>> d314fef6c656e3aacafe64e032cca0f4851112c9
 		return (error("permission denied", name), *stat = 0, -1);
->>>>>>> 4ce88c5298f396b92f04ecf993075efb61c76905
 	if (trunc)
 		fd = open(name, O_CREAT | O_WRONLY | O_TRUNC, 0777);
 	else
