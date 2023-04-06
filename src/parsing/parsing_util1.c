eea1e61d67b34c08e85b6cc130b3a30c87d24929
Abderrahim Jari <ajari@e1r12p10.1337.ma>
2023-04-06 15:27:41 +0000

    set_ajari

--- src/parsing/parsing_util1.c
+++ src/parsing/parsing_util1.c
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing_util1.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aahrach <aahrach@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ajari <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 10:08:41 by ajari             #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/04/06 15:25:43 by aahrach          ###   ########.fr       */
-=======
-/*   Updated: 2023/04/06 15:15:11 by ajari            ###   ########.fr       */
->>>>>>> fdd0fb926ae034f06dd522cd68120b9a7bb7908c
+/*   Updated: 2023/04/06 15:27:40 by ajari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,17 +39,10 @@ void	lstfree(t_list *list, t_env *env, t_var *var)
 		list = tmp;
 	}
 	g_v = malloc(sizeof(t_list));
-<<<<<<< HEAD
-    if (!g_v)
-        exit(error("error of allocation", ""));
-    g_v->env = env;
-    g_v->var = var;
-=======
 	if (!g_v)
 		exit(error("error of allocation", ""));
 	g_v->env = env;
 	g_v->var = var;
->>>>>>> fdd0fb926ae034f06dd522cd68120b9a7bb7908c
 }
 
 void	add_char(char **s, char c)
