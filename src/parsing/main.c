d56044ada5bf0a1c6867aab78fd8bff43f9a038d
Achraf Ahrach <aahrach@e2r12p5.1337.ma>
2023-04-07 10:26:42 +0000

    set_aahrach

--- src/parsing/main.c
+++ src/parsing/main.c
@@ -6,11 +6,7 @@
 /*   By: aahrach <aahrach@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 12:27:58 by aahrach           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/04/07 10:06:21 by aahrach          ###   ########.fr       */
-=======
-/*   Updated: 2023/04/07 10:06:41 by ajari            ###   ########.fr       */
->>>>>>> 32bce1a4fe9f898f952c9a999016cbb57d255d2c
+/*   Updated: 2023/04/07 10:26:38 by aahrach          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,6 +78,30 @@ void	shlvl(t_env **env)
 	}
 }
 
+void	printf_list(t_list *tem)
+{
+	char	ss[1000];
+
+	while (tem)
+	{
+		printf("<<<<<<<<<<<<<<<<pipe>>>>>>>>>>>>>>>>>>>>>\n");
+		for (int i = 0; tem->cmdsp && tem->cmdsp[i]; i++)
+		{
+			if (!i)
+				printf("cmd:");
+			printf("#%s# ", tem->cmdsp[i]);
+		}
+		printf("\nstat:%d\ninfile:%d\n", tem->stat, tem->i_f);
+		if (0 && tem->i_f != -1 && tem->i_f != -2)
+		{
+			read(tem->i_f, ss, 100);
+			printf("%s\n", ss);
+		}
+		printf("outfile:%d\n", tem->o_f);
+		tem = tem->next;
+	}
+}
+
 int	main(int ac, char **av, char **ev)
 {
 	char	*s;
@@ -105,6 +125,7 @@ int	main(int ac, char **av, char **ev)
 		if (!check_in(s))
 			continue ;
 		fill_cmds(s, env, var);
+		printf_list(g_v);
 		execution();
 		env = g_v->env;
 		lstfree(g_v, env, var);
